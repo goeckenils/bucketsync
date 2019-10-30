ce01de410a85638a820f8510aa1f1b598544b65b
Nils Goecke <nils.goecke98@gmail.com>
2019-10-30 12:51:46 +0100

    build header & Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Logo from '../images/LogoIcon.svg';
+import { PrimaryButton } from './Button';
+
+const Hero = () => (
+	<Container>
+		<Background className="custom-bg">
+			<Wrapper>
+				<TextWrapper>
+					<Title>Backup Recovery Service</Title>
+					<SubTitle>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repellendus magni repellat. Eum
+						veniam a vel ipsam! Ipsam, facere repellendus.
+					</SubTitle>
+					<PrimaryButton margin={5px}>More</PrimaryButton>
+				</TextWrapper>
+				<LogoWrapper>
+					<IconLogo />
+				</LogoWrapper>
+			</Wrapper>
+		</Background>
+	</Container>
+);
+
+export default Hero;
+
+export const Container = styled.div`
+	width: 100vh;
+	height: 100vh;
+	z-index: 0;
+`;
+
+export const Background = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+`;
+
+export const Wrapper = styled.div`
+	width: 1100px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const Title = styled.h1`
+	font-size: 45px;
+	color: #fff;
+`;
+
+export const SubTitle = styled.h3`
+	font-size: 18px;
+	color: #fff;
+`;
+export const Text = styled.h3`
+	font-size: 14px;
+	color: #fff;
+	margin-top: 10px;
+`;
+
+export const TextWrapper = styled.div`width: 600px;`;
+const LogoWrapper = styled.div``;
+
+const float = keyframes`
+    0% {
+		box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+		transform: translatey(0px);
+	}
+	50% {
+		box-shadow: 0 25px 15px 0px rgba(0,0,0,0.2);
+		transform: translatey(-4px);
+	}
+	100% {
+		box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+		transform: translatey(0px);
+	}
+`;
+
+const IconLogo = styled(Logo)`
+    width: 400px;
+    height: 400px;
+
+    
+        path  {
+            animation: ${float} 6s ease-in-out infinite;
+            transition: all 1s ease-in-out;
+            transform: translatey(0px);
+            box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+        }
+`;
---
+++ src/components/navigation.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from '../images/Logo.svg';
+import { PrimaryButton } from './Button';
+
+const Navigation = () => (
+	<NavWrapper>
+		<Container>
+			<Menu>
+				<LogoWrapper>
+					<EditLogo />
+				</LogoWrapper>
+				<ItemWrapper>
+					<MenuItem>
+						<Link to="/">
+							<Titles>Technologie</Titles>
+						</Link>
+					</MenuItem>
+					<MenuItem>
+						<Link to="/">
+							<Titles>Service</Titles>
+						</Link>
+					</MenuItem>
+					<MenuItem>
+						<Link to="/">
+							<Titles>Referenzen</Titles>
+						</Link>
+					</MenuItem>
+					<MenuItem>
+						<PrimaryButton>Contact us</PrimaryButton>
+					</MenuItem>
+				</ItemWrapper>
+			</Menu>
+		</Container>
+	</NavWrapper>
+);
+
+export default Navigation;
+
+export const NavWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	position: fixed;
+	background: #fff;
+	width: 100%;
+	align-items: center;
+	z-index: 100;
+	box-shadow: rgba(12, 0, 46, 0.06) 0px 4px 6px 0px;
+`;
+export const Container = styled.div`width: 1100px;`;
+export const Menu = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+export const MenuItem = styled.div`padding: 5px 10px;`;
+export const LogoWrapper = styled.div`padding: 10px 20px;`;
+export const EditLogo = styled(Logo)`
+width: 180px;
+height: 55px;
+`;
+export const ItemWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const Titles = styled.h3`
+	font-size: 16px;
+	color: #58595b;
+
+	&:hover {
+		text-decoration: underline;
+	}
+`;
